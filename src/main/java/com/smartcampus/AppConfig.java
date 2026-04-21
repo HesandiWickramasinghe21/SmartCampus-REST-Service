7af56104f925dc30a20f0cefa080e115570a2482
Hesandi Wickramasinghe <hesandi.20242159@ii.ac.lk>
2026-04-21 11:21:52 +0800

    Initial project setup with Jakarta EE and Tomcat configuration

---
+++ src/main/java/com/smartcampus/AppConfig.java
@@ -0,0 +1,12 @@
+package com.smartcampus;
+
+import javax.ws.rs.ApplicationPath;
+import org.glassfish.jersey.server.ResourceConfig;
+
+@ApplicationPath("/api/v1")
+public class AppConfig extends ResourceConfig {
+
+    public AppConfig() {
+        packages("com.smartcampus");
+    }
+}
